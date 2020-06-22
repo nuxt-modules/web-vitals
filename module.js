c4aa54657a57ac4c324d00b4618a7694f6404afa
Tobias Cichon <tobiascichon@gmail.com>
2020-06-22 15:25:35 +0700

    Add `disabled` option to plugin configuration

--- module.js
+++ module.js
@@ -9,6 +9,11 @@ export default function (moduleOptions) {
   if (!options.debug) {
     options.debug = 0
   }
+  
+  // Don't add plugin if disabled
+  if (this.options.disabled) {
+    return;
+  }  
 
   this.addPlugin({
     src: resolve(__dirname, 'plugin.js'),
