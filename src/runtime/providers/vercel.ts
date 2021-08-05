c8c02806ae97fab8defb948c2e056b35ef63baa8
iranagame <34863253+iranagame@users.noreply.github.com>
2021-08-06 02:59:26 +0430

    Update vercel.ts
    Correct vercel endpoint to fix URL format issue

--- src/runtime/providers/vercel.ts
+++ src/runtime/providers/vercel.ts
@@ -5,7 +5,7 @@ export interface Options {
   debug: boolean
 }
 
-const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals'
+const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals?d=1'
 
 export function sendToAnalytics ({ fullPath, href }, metric, options: Options) {
   const body = {
