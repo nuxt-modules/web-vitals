3ef3492971f5373051150a8621ee3a176dd58e10
Daniel Roe <daniel@roe.dev>
2022-07-26 11:18:57 +0100

    fix: handle vue-router 4x syntax

--- src/runtime/vitals.client.mjs
+++ src/runtime/vitals.client.mjs
@@ -3,17 +3,26 @@ import { sendToAnalytics } from '~vitals-provider'
 
 const options = <%= JSON.stringify(options, null, 2) %>
 
-export default function ({ app: { router } }) {
-  router.onReady((to) => {
-    webVitals({
-      options,
-      sendToAnalytics,
-      route: to
+const sendVitals = (to) => webVitals({
+  options,
+  sendToAnalytics,
+  route: to
+})
+
+export default async function (ctx) {
+  const router = ctx.app?.router || ctx.$router
+  if (!router) { return }
+
+  if ('isReady' in router) {
+    // vue-router 4
+    await router.isReady()
+    sendVitals(router.currentRoute)
+    router.afterEach((to) => sendVitals(to))
+  } else {
+    // vue-router 3
+    router.onReady((to) => {
+      sendVitals(to)
+      router.afterEach((to) => sendVitals(to))
     })
-    router.afterEach(to => webVitals({
-      options,
-      sendToAnalytics,
-      route: to
-    }))
-  })
+  }
 }
