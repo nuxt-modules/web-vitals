9db765aca91ab0792d71d7531c1d9d70244dc733
Pooya Parsa <pooya@pi0.io>
2022-11-29 13:50:22 +0100

    fix loh

--- playground/server/api/web-vitals.ts
+++ playground/server/api/web-vitals.ts
@@ -1,5 +1,5 @@
 export default eventHandler(async (event) => {
   const body = await readBody(event)
-  console.log('Web-vitals event:' + body)
+  console.log('Web-vitals event:', JSON.stringify(body))
   return 'OK'
 })
