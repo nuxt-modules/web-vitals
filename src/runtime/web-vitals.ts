dda920542f5f8cc389e03922ccb2ca7af09b7351
Hossein Shakouri <mhshakouri@gmail.com>
2023-07-04 22:45:48 +0330

    added getINP to web-vitals exports

--- src/runtime/web-vitals.ts
+++ src/runtime/web-vitals.ts
@@ -1,7 +1,7 @@
 
 import { logError } from './utils'
 
-export async function webVitals ({ route, options, sendToAnalytics }) {
+export async function webVitals({ route, options, sendToAnalytics }) {
   const context = {
     fullPath: route.fullPath,
     href: location.href
@@ -13,12 +13,13 @@ export async function webVitals ({ route, options, sendToAnalytics }) {
   // }
 
   try {
-    const { getCLS, getFID, getLCP, getTTFB, getFCP } = await import('web-vitals').then((r: any) => r.default || r)
+    const { getCLS, getFID, getLCP, getTTFB, getFCP, getINP } = await import('web-vitals').then((r: any) => r.default || r)
     getFID(metric => sendToAnalytics(context, metric, options))
     getTTFB(metric => sendToAnalytics(context, metric, options))
     getLCP(metric => sendToAnalytics(context, metric, options))
     getCLS(metric => sendToAnalytics(context, metric, options))
     getFCP(metric => sendToAnalytics(context, metric, options))
+    getINP(metric => sendToAnalytics(context, metric, options))
   } catch (err) {
     logError(err)
   }
